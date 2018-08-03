7f3a9a33e8cef6757cc8428676e7c0efa1ade19d
Robert Ford <robfordww@gmail.com>
2018-08-03 19:37:42 +0200

    Delete README.mk

--- README.mk
+++
@@ -1,9 +0,0 @@
-# The RunXML library 
-
-RunXML library is a Go XML library, with focus on speed.  
-It is mostly based on the [RapidXML][rx] by Marcin Kalicinski.
-
-#### WARNING: Work in progress
-This library is WIP, do not use it for anyting at this moment.
-
-[rx]: http://rapidxml.sourceforge.net/
